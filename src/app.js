bdb1ba5915c87c3d6a25b017d363c70f98912ea5
Thomas Wilkinson <tbondwilkinson@gmail.com>
2020-08-17 23:35:53 -0500

    Use hardcoded /timestamp/burn-timecode

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ class App extends React.Component {
           </div>
           <form
             name="burn-in-time-code"
-            action="burn-timecode"
+            action="/timestamp/burn-timecode"
             method="POST"
             enctype="multipart/form-data"
             onSubmit={this.handleSubmit}>
